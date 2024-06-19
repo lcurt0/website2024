37aa8bc2cdf9e6d49c5455b6d2eb4e1e98988f99
lcurt0 <109284131+lcurt0@users.noreply.github.com>
2024-06-19 14:18:18 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,38 +1,19 @@
-<form action="form.php" method="POST" class="contact__form">
-    <div class="name">
-        <label for="name">Name <span class="required">*</span></label>
-        <br>
-        <input type="text" name="name" required>
-    </div>
-    <div class="email">
-        <label for="email">Email <span class="required">*</span></label>
-        <br>
-        <input type="email" name="email" required>
-    </div>
-    <div class="textarea">
-        <label for="description">Tell me a bit about your project <span class="required">*</span></label>
-        <br>
-        <textarea name="description" required></textarea>
-    </div>
-    <div class="hear">
-        <label for="hear">How did you hear about me?</label>
-        <br>
-        <input type="text" name="hear">
-    </div>
-    <div class="btn">
-        <button type="submit" value="submit" id="submit" name="SubmitButton" class="btn__active submit__btn">
-            <a class="fonH4">
-                <svg width="21" height="21" viewBox="0 0 21 21" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M16.3825 3.88326H0V0H20.2658V20.2658H16.3825V3.88326Z" fill="" />
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M-0.000202745 16.3926C7.15253 16.3926 12.951 10.5941 12.951 3.44141L16.8242 3.44141C16.8242 12.7333 9.29167 20.2658 -0.000202914 20.2658L-0.000202745 16.3926Z"
-                        fill="" />
-                </svg>
-                Send
-            </a>
-        </button>
-    </div>
-</form>
-
+<?php
+    if(isset($_POST['SubmitButton'])){ //check if form was submitted
+        
+        $receiver = 'lautarocurto@gmail.com';
+        
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $description = $_POST['description'];
+        $launch = $_POST['launch__date'];
+        $budget = $_POST['budget'];
+        $hear = $_POST['hear'];
+        $header = "WEB INQUIRY";
+        $content = "\n Email: " . $email . "\n About our project: " . $description . "\n Our ideal launch date is: " . $launch . "\n Our budget is: " . $budget . "\n We heard about you through " . $hear;
+        
+        mail($receiver, $name, $content, $header);
+        header("Location: https://lautarocurto.com/contact.php");
+        echo("<h2 class='formMsg'>Message sent!</h2>");
+    } 
+?>
